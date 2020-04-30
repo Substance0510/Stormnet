4ca530946726fc005b85a2d8e3a400428de3e318
anton <Substance0510@gmail.com>
2020-05-01 02:16:30 +0300

    Добавил на главную страницу некое подобие навигации по сайту.

--- Lection9/Dj_project/blog/urls.py
+++ Lection9/Dj_project/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('blog/', views.post_list, name='post_list'),
-    path('homework/', views.homework, name='homework')
+    path('homework/', views.homework, name='homework'),
+    path('ded/', views.ded_moroz, name='ded_moroz'),
 ]
--- Lection9/Dj_project/blog/views.py
+++ Lection9/Dj_project/blog/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from . models import Post
 
-# Create your views here.
-
 
 def main_page(request):
-    return render(request, 'blog/main_page.html', {})
+    return render(request, 'blog/main_page.html')
 
 
 def post_list(request):
@@ -15,11 +13,21 @@ def post_list(request):
 
 def homework(request):
     name = request.GET.get('name')
+    if name is None:
+        name = 'John Doo'
+    print(name)
     type = request.GET.get('type')
+    if type is None:
+        type = 'Unknown'
     humanoid = request.GET.get('humanoid')
+    if humanoid is None:
+        humanoid = 'I don\'t know :('
     context = {'name': name,
-               'name_lenght': len(name),
+               'name_lenght': len(name.replace(' ', '')),
                'name_reversed': name[::-1],
                'type': type,
                'humanoid': humanoid}
     return render(request, 'blog/homework.html', context=context)
+
+def ded_moroz(request):
+    return render(request, 'blog/ded_moroz.html')
